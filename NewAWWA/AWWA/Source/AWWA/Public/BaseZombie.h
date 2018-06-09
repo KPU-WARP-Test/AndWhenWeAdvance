dc157ec7162d6c9f469d7eaae9e7961a69023450
GameForPeople <koreagamemaker@gmail.com>
2018-06-09 22:36:44 +0900

    [DEV] Create "BaseZombieClass & BaseZombieBP" // DEV_90
    [DEV_90] BaseZombie Class 생성 및 BP확장

---
+++ NewAWWA/AWWA/Source/AWWA/Public/BaseZombie.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseZombie.generated.h"
+
+UCLASS()
+class AWWA_API ABaseZombie : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseZombie();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "ZombieType")
+	int32 m_ZombieType;
+	// It Use Zombie Stat Define???? -> Not Use
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "ZombieType")
+	int32 m_MeshBuild;
+	// It Use Zombie's Sketal Mesh Define?
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stat")
+	int32 m_State;
+	// Zombie's HP
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stat")
+	int32 m_hp;
+	// Zombie's HP
+
+	//UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stat")
+	//int32 m_speed;
+	//// Zombie's Speed
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Stat")
+	int32 m_damage;
+	// Zombie's Damage
+};
